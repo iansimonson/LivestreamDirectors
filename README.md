e6d710c1836b2ac56077a5628dcfbec4fd4a8adf
Ian Simonson <ian.simonson@vanderbilt.edu>
2015-06-19 16:58:49 -0400

    list directors completed for now

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ Livestream Directors
 ======
 
 This is a small API built for Livestream which allows users with Livestream accounts to register as a film director. Directors can list their favorite camera, and favorite films as well as look up fellow directors.
+
+API Endpoints
+------
+
+Server Representation
+------
+
+The directors are stored in a mySQL table with the following columns
+
+1. livestream_id - integer, and primary key (pulled from livestream account info)
+2. full_name - varchar(255), (also pulled from livestream account info)
+3. dob - datetime, the date of birth (also pulled from account info)
+4. favorite_camera - varchar(255), the director's favorite camera as a string
+5. favorite_films - text, a JSON string listing the director's favorite film.
--- server.js
+++ server.js
@@ -14,27 +14,61 @@ var port = process.argv[2];
 var username = process.argv[3];
 var pswd = process.argv[4];
 
+var pool = mysql.createPool({
+  user: username,
+  password: pswd,
+  database: 'director_registry'
+});
 
 // Create the server
-http.createServer(function(req,res){
+http.createServer(requestHandler).listen(port);
+console.log('Server running, listening on port: ' + port);
+
+
+
+/***********************
+ FUNCTION: requestHandler(request, response)
+ This function handles incoming requests to the http server
+ Handled entry points:
+  GET /directors -> lists all directors registered
+  POST /new livestream_id -> registers a new director using livestream_id
+  POST /favcam livestream_id favorite_camera [auth] -> updates the favorite camera for the given livestream_id 
+  POST /updatefilms [auth] { type:add/remove/replace films:[list,of,films] } -> updates the favorite films list
+************************/
+
+function requestHandler(req,res){
+  var reqObj = url.parse(req.url,true);
+
   if(req.method === 'GET'){
-    var connection = mysql.createConnection({
-      user: username,
-      password: pswd,
-      database: 'director_registry'
-    });
+    if(req.url === '/directors'){
+      pool.getConnection(function(err,conn){
+        if(err){
+          console.error('error connecting: ' + err);
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.end('CONNECTION FAILED');
+          return;
+        }
 
-    connection.connect(function(err){
-      if(err){
-        console.error('error connecting: ' + err);
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('CONNECTION FAILED');
-        return;
-      }
+        console.log('connected as id: ' + conn.threadId);
 
-      console.log('connected as id: ' + connection.threadId);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('connection established');
-    });
+        conn.query('SELECT * FROM directors',function(err,results,fields){
+          if(err) return console.error(err);
+
+          res.writeHead(200,{'Content-Type': 'application/JSON'});
+          results.forEach(function(row){
+            console.log(JSON.stringify(row));
+            res.write(JSON.stringify(row) + '\n');
+          });
+          conn.release();
+          res.end();
+        });
+
+      });
+    } else {
+      res.writeHead(404,{'Content-Type': 'text/plain'});
+      res.end('Can only GET on /directors');
+    }
+  } else if (req.method === 'POST'){
+    
   }
-}).listen(port);
+}
